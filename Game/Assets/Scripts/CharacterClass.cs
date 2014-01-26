bf9f73a2b50a9b0043560c2e5c5f314bb40d5cf1
willis <willfung616@hotmail.com>
2014-01-26 14:28:27 -0800

    Endgame logic going to gameboard

--- Game/Assets/Scripts/CharacterClass.cs
+++ Game/Assets/Scripts/CharacterClass.cs
@@ -9,7 +9,6 @@ public class CharacterClass : MonoBehaviour {
 	TileClass nextTile;
 	float t = 0;
 	private bool isDead = false;
-	private float endTimer = 3f;
 	public AudioClip deathSound;
 
 	// Use this for initialization
@@ -32,13 +31,6 @@ public class CharacterClass : MonoBehaviour {
 	void Update () {
 		transform.position = Vector2.Lerp(currentTile.transform.position, nextTile.transform.position, t);
 		t+=Time.deltaTime;
-
-		if (isDead) {
-			endTimer -= Time.deltaTime;
-			if (endTimer < 0) {
-				EndGame ();
-			}
-		}
 	}
 
 	public void Move(int direction) {
@@ -98,14 +90,11 @@ public class CharacterClass : MonoBehaviour {
 		Debug.Log (tag + " Died");
 		isDead = true;
 		gameboard.GetTileAtCoordinate(transform.position.x, transform.position.y).GetComponent<TileClass>().entity = null;
-//		Destroy (gameObject);
-	}
 
-	private void EndGame() {
 		if(tag == "P1"){
-			Application.LoadLevel (4);
+			gameboard.EndGame(1);
 		}else if(tag == "P2"){
-			Application.LoadLevel (3);
+			gameboard.EndGame(2);
 		}
 		tag = "Untagged";
 		foreach(MonoBehaviour mb in GetComponents<MonoBehaviour>()){
