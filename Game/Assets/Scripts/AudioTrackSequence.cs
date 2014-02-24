a946caee55ae7080544ed1bfeb3ddcf34970eb7b
Howie Chan <hw.chan@hotmail.com>
2014-02-23 16:48:02 -0800

    New music loops

---
+++ Game/Assets/Scripts/AudioTrackSequence.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioTrackSequence : MonoBehaviour {
+
+	public AudioSource audioStart;
+	public AudioSource audioLoop;
+	public float trackTransitionTime = -.15f;
+
+	// Use this for initialization
+	void Start () {
+		AudioSource[] tracks = GetComponents<AudioSource>();
+		audioStart = tracks[0];
+		audioLoop = tracks[1];
+		StartCoroutine(MusicSequence());
+	}
+
+	IEnumerator MusicSequence(){
+		audioStart.Play();
+		yield return new WaitForSeconds(audioStart.clip.length + trackTransitionTime);
+		print ("loop");
+		audioLoop.Play();
+	}
+}
