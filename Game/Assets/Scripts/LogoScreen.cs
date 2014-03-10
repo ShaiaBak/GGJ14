52c797ed1450fdfbb58949efd01dc2564e01baa3
slushee <tylerpham.9@gmail.com>
2014-03-09 19:17:29 -0800

    more splash screen stuff, fixed game board sound

---
+++ Game/Assets/Scripts/LogoScreen.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LogoScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void LoadGame () {
+		Application.LoadLevelAdditive("MainMenu");
+	}
+
+}
